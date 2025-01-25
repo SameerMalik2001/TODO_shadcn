09c78cecf282f8ee3994249dca822195c65f3de7
Sameer Malik <sameer7417277576@gmail.com>
2025-01-25 20:49:26 +0530

    all code

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,101 +1,48 @@
-import Image from "next/image";
+'use client'
+
+import { useEffect, useState } from "react";
+import InputBox from "./compoents/InputBox";
+import { TODOTYPE } from "./types/types";
+import TablePart from "./compoents/TablePart";
+
+
 
 export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+  const [Todos, setTodos] = useState<TODOTYPE[]>([])
+  const [inputValue, setInputValue] = useState<string>('')
+  const [editID, setEditID] = useState<string>('')
+  const [checkAllState, setCheckAllState] = useState<boolean>(false)
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
-  );
+  useEffect(()=> {
+    if(typeof window !== 'undefined') {
+      const tempTodos = JSON.parse(localStorage.getItem('todos') || '[]')
+      setTodos(tempTodos)
+    }
+  }, [])
+
+  const completionUpdate = (e: string | boolean, id: string) => {
+    if(typeof e === 'boolean' && id) {
+      let tempTodo = [...Todos]
+     
+        setCheckAllState(false)
+
+      tempTodo.map((todo: TODOTYPE)=> {
+        if(todo?.id === id) {
+          todo.completed = e
+          return todo
+        }
+      })
+      setTodos(tempTodo)
+      localStorage.setItem('todos', JSON.stringify(tempTodo))
+    }
+  }
+
+  console.log(Todos);
+	return <div className='bg-white min-h-screen w-screen p-10 text-black'>
+    <h1 className="font-[500] text-[28px]">TODO APPLICATION</h1>
+
+    <InputBox setCheckAllState={setCheckAllState} editID={editID} setEditID={setEditID} setTodos={setTodos} Todos={Todos} setInputValue={setInputValue} inputValue={inputValue}/>
+
+    <TablePart Todos={Todos} setTodos={setTodos} checkAllState={checkAllState} setCheckAllState={setCheckAllState} setEditID={setEditID} setInputValue={setInputValue} completionUpdate={completionUpdate}/>
+  </div>;
 }
